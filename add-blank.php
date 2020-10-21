7509b0f26856530870767b36f7545707798c8cae
橋本和可奈 <wkn.0d41@gmail.com>
2020-10-21 11:47:00 +0900

    現状

--- add-blank.php
+++ add-blank.php
@@ -4,4 +4,14 @@ Plugin Name: add-blank
 Description: 投稿の中の外部リンク全てに、target="_blank"をつける。
 Author: Wakana Hashimoto
 */
+
+add_filter('the_content','add_blank');
+
+function add_blank($content){
+	// preg_match_all('/<a')
+	$content = preg_replace('/<a.+\/a>/','リンク',$content);
+	return $content;
+}
+
+
 ?>
