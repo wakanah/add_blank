b11d74fd06d9b25357259ac3ff2b1eef8d02a1dc
橋本和可奈 <wkn.0d41@gmail.com>
2020-10-21 14:50:09 +0900

    外部リンク判定

--- add-blank.php
+++ add-blank.php
@@ -8,10 +8,14 @@ Author: Wakana Hashimoto
 add_filter('the_content','add_blank');
 
 function add_blank($content){
-	preg_match_all('/<a.+\/a>/u',$content,$atag);
-	$atag = $atag[0];
-	print_r($atag);
-	$content = preg_replace('/<a.+\/a>/','リンク',$content);
+	preg_match_all('/<a.+\/a>/u',$content,$atags);
+	$atags = $atags[0];
+	print_r($atags);
+	foreach ($atags as $atag) {
+		if(preg_match('/https?\:\/\//',$atag) && !strpos($atag,$_SERVER["HTTP_HOST"])){
+			$content = str_replace($atag,'外部リンク',$content);
+		}
+	}
 	return $content;
 }
 
